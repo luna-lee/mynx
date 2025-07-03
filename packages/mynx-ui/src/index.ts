6603493a4fefc03344a5532ba7cec8cfeee94b20
lip_mac <1724956493@qq.com>
2025-07-03 08:50:51 +0800

    🦄 refactor:
    移除多余代码

--- packages/mynx-ui/src/index.ts
+++ packages/mynx-ui/src/index.ts
@@ -3,21 +3,13 @@ import type { App } from 'vue';
 // Import components
 import MHierarchy from './MHierarchy/index.vue';
 import MCurtain from './MCurtain/index.vue';
-// @ts-ignore
 import MList from './MList/index.vue';
-// @ts-ignore
 import MDiv from './MDiv/index.vue';
-// @ts-ignore
 import MScale from './MScale/index.vue';
-// @ts-ignore
 import MTextellips from './MTextellips/index.vue';
-// @ts-ignore
 import MIfShow from './MIfShow/index.vue';
-// @ts-ignore
 import MValidator from './MValidator/index.vue';
-// @ts-ignore
 import MScroll from './MScroll/index.vue';
-// @ts-ignore
 import MSubsection from './MSubsection/index.vue';
 
 // Create component map
